b971778296e5ca7b3277931d1e4d6e060b43a7be
GulameRaza <60345094+GulameRaza@users.noreply.github.com>
2024-06-15 11:24:40 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = null
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//when convert in number
+// '33' => 33
+// 'tugb323' => NaN
+// undefined => NaN
+// null => 0
+// true => 1
+// false => 0
+
+let isLoggedIn = undefined
+
+isLoggedIn = Boolean(isLoggedIn)
+console.log(isLoggedIn);
+
+// 1 => true
+//0 => false
+// 'faizal' => true
+// '' => false
+// null => false
+// undefined => false
+
+
